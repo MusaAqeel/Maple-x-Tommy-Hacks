c2d0d7b9803c2b45070e174a9acddda52ba7f0bf
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2022-12-21 21:31:37 -0500

    Logo

--- components/Shared/Logo.js
+++ components/Shared/Logo.js
@@ -1,4 +1,4 @@
-import MooseLogo from "/public/static/UI/Logo.svg";
+import HacksLogo from "/public/static/UI/Logo.png";
 import Image from "next/image";
 
 // TO EDIT SIZE OF LOGO, DO NOT EDIT IMAGE DIMENSIONS DIRECTLY, EDIT CONTAINER WIDTH WRAPPED AROUND
@@ -7,9 +7,9 @@ const Logo = (props) => {
   return (
     <>
       <Image
-        src={MooseLogo}
+        src={HacksLogo}
         layout="intrinsic"
-        alt="UofTHacks IX Logo"
+        alt="Hacks Logo"
         priority
       />
     </>
