045623834f854bd1cbe45fa0b1859addf6011e80
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2022-12-21 21:18:51 -0500

    Update Mountains.js

--- components/Other/Mountains/Mountains.js
+++ components/Other/Mountains/Mountains.js
@@ -21,25 +21,7 @@ import Art4 from "/public/static/Mountains/4.svg";
 const Mountains = () => {
   return (
     <>
-      <Layer0>
-        <Image src={Art0} layout="responsive" alt="Mountain-Layer0" priority />
-      </Layer0>
-      <Layer1>
-        <Image src={Art1} layout="responsive" alt="Mountain-Layer1" priority />
-        <Layer1Cover />
-      </Layer1>
-      <Layer2>
-        <Image src={Art2} layout="responsive" alt="Mountain-Layer2" priority />
-        <Layer2Cover />
-      </Layer2>
-      <Layer3>
-        <Image src={Art3} layout="responsive" alt="Mountain-Layer3" priority />
-        <Layer3Cover />
-      </Layer3>
-      <Layer4>
-        <Image src={Art4} layout="responsive" alt="Mountain-Layer4" priority />
-        <Layer4Cover />
-      </Layer4>
+
     </>
   );
 };
