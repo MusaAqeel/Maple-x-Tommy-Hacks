19c861fc819ba6abf9a0e38982dc0fd6ef9be79e
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2022-12-22 13:14:00 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -96,7 +96,7 @@ import Footer from "../components/Layout/Footer/Footer";
 
 const Parallax = styled.section`
   position: absolute;
-  background: linear-gradient(180deg, #FFFFFF 0%, #FFFFFF 100%);
+  background: linear-gradient(180deg, #FFFFFF 0%, ##A3A1A2 100%);
   perspective: 100px;
   left: 0;
   bottom: 0;
