5870fdecd3ce91a4b6ac3d0e67fea0e88f9e25b6
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2022-12-23 16:01:25 -0500

    Favicon

--- pages/index.js
+++ pages/index.js
@@ -125,19 +125,19 @@ const Home = () => {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/favicon-32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="48x32"
-            href="/favicon-32x32.png"
+            href="/favicon-32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/favicon-16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
           <title>Maple X Tommy Hacks </title>
