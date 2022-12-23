416c6f8b2ba6b1eb0dddf14d5b82c572c41f52de
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2022-12-23 00:08:05 -0500

    Update Divider2.js

--- components/Shared/Divider/Divider2.js
+++ components/Shared/Divider/Divider2.js
@@ -9,7 +9,7 @@ const Divider2 = () => {
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 1920 155"
     preserveAspectRatio="none"
-    transform="scaleY(-1)"
+
   >
     <path
       d="M 0 0 H 1920 V 180 C 1720 183 1720 183 895 180 C 895 177 895 179 521 179 C 163 182 166 180 0 180 V 13 Z"
