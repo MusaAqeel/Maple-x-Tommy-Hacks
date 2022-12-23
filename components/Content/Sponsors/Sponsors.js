f95759da624cc3b466a91b3322746753d3d88eff
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2022-12-22 23:54:20 -0500

    Update Sponsors.js

--- components/Content/Sponsors/Sponsors.js
+++ components/Content/Sponsors/Sponsors.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Divider from "../../Shared/Divider/Divider";
-import Divider2 from "../../Shared/Divider2/Divider2";
+import Divider2 from "../../Shared/Divider/Divider2";
 import {
   Wrapper,
   Header,
