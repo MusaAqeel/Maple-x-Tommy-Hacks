c23795b6041358a0575d0758de0e994f01b025e8
MusaAqeel <77996753+MusaAqeel@users.noreply.github.com>
2022-12-22 23:56:01 -0500

    x-1

--- components/Shared/Divider/Divider2.js
+++ components/Shared/Divider/Divider2.js
@@ -9,7 +9,7 @@ const Divider2 = () => {
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 1920 155"
     preserveAspectRatio="none"
-    transform="scale(-1, 1)"
+    transform="scaleX(-1)"
   >
     <path
       d="M0 0H1920V89.3302C1920 89.3302 1717.5 154.519 1405 154.519C1092.5 154.519 878.455 89.3302 520 89.3302C161.545 89.3302 0 155 0 155V0Z"
@@ -17,6 +17,7 @@ const Divider2 = () => {
     />
   </Shape>
 </Wrapper>
+
 
   );
 };
